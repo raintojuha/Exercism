dded2d2f456cf0f7a865f6650fe3f9cbb2648aff
raintojuha <raintojuha@gmail.com>
2023-05-10 05:51:41 +0300

    Completed Java lasagna excercise

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,29 @@
 public class Lasagna {
     // TODO: define the 'expectedMinutesInOven()' method
+	public int expectedMinutesInOven() {
+		return 40;
+	}
+	
+	
+	
+	public int remainingMinutesInOven(int elapsedTime) {
+		return expectedMinutesInOven() - elapsedTime;
+	}
+	
+	public int preparationTimeInMinutes(int layers) {
+		int minutesPerLayer = 2;
+		return minutesPerLayer * layers;
+	}
+	
+	public int totalTimeInMinutes(int layers, int elapsedTime) {
+		return preparationTimeInMinutes(layers) + elapsedTime;
+	}
 
     // TODO: define the 'remainingMinutesInOven()' method
 
     // TODO: define the 'preparationTimeInMinutes()' method
 
     // TODO: define the 'totalTimeInMinutes()' method
+	
+	
 }
