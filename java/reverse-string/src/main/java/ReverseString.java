da0baf3356a83d70aab89d1bbcd5be5693b12dfc
raintojuha <raintojuha@gmail.com>
2023-05-11 21:02:37 +0300

    Completed ReverseString exercise.

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,20 @@
 class ReverseString {
-
-    String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	/**
+	 * Method to reverse a string
+	 * 
+	 * This solution uses only String methods. While a char array was my first idea,
+	 * chars have their own module in Exercism with its own Reverse String exercise.
+	 */
+    static String reverse(String inputString) {        
+        // New string for reversed result
+        String newString = "";
+       
+        for(int i = 0; i < inputString.length(); i++) {
+        	// Use substring() to isolate one character and add it to the new string
+        	newString += inputString.substring(inputString.length()-i-1, inputString.length()-i);
+        }
+        
+        return newString;
+       
     }
-  
 }
