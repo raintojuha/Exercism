702ca5719f0d76ba1eb0cd215813f9915ef8057e
raintojuha <raintojuha@gmail.com>
2023-05-11 08:09:47 +0300

    Completed AnnalynsInfiltration excercise

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -1,17 +1,51 @@
 class AnnalynsInfiltration {
+	
+	/**
+	 * If knight is awake - can not fast attack
+	 */
     public static boolean canFastAttack(boolean knightIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFastAttack() method");
+    	if(knightIsAwake) {
+    		return false;
+    	} else {
+    		return true;
+    	}
     }
 
+    /**
+     * If any one of the three are awake - can spy
+     */
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSpy() method");
+        if(knightIsAwake || archerIsAwake || prisonerIsAwake) {
+        	return true;
+        } else {
+        	return false;
+        }
     }
 
+    /**
+     * If prisoner is awake but acher is not - can 
+     */
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSignalPrisoner() method");
+    	if(prisonerIsAwake && !archerIsAwake) {
+    		return true;
+    	} else {
+    		return false;
+    	}
     }
 
+    /**
+     * If pet dog is present
+     * 		- archer must be asleep
+     * 
+     * If pet dog not present
+     * 		- prisoner must be awake
+     * 		- knight and arcer must be asleep
+     */
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
+    	if(petDogIsPresent && !archerIsAwake || prisonerIsAwake && !knightIsAwake && !archerIsAwake) {
+    		return true;
+    	} else {
+    		return false;
+    	}
     }
 }
