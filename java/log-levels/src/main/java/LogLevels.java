210c9d98c6b5976f61b4ab889f1e8e3d8fedda15
raintojuha <raintojuha@gmail.com>
2023-05-11 07:46:00 +0300

    Completed LogLevels excercise

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -2,13 +2,43 @@ import java.lang.*;
 
 public class LogLevels {
     
+	
+	/**
+	 * Get message from the logline
+	 * Remove whitespaces
+	 * Return message string
+	 */
     public static String message(String logLine) {
-    	String msg = logLine.replace("\r", "").replace("\n", ""); // Replace white spaces
     	
-    	return msg.split("]: ")[1]; // Use ']: ' as a split point to get message
+    	// Remove character escaping
+    	String msg = logLine.replace("\r", "") // carriage return
+    						.replace("\n", "") // new line
+    						.replace("\t", ""); // tabulator
+    	
+    	// Use ']: ' as a split point to sepaprate message
+    	msg = msg.split("]: ")[1];
+    	
+    	// While the first character is a white space
+    	while(msg.charAt(0) == 32) {
+    		// Create a new string ignoring that whitespace
+    		msg = msg.substring(1, msg.length());
+    	}
+    	
+    	// While the last character is a white space
+    	while(msg.charAt(msg.length() - 1) == 32) {
+    		// Create a new string ignoring that whitespace
+    		msg = msg.substring(0, msg.length() - 2);
+    	}
+    	
+    	return msg; 
     }
 
+    /**
+     * Look for [LEVEL] in the logline
+     * Return plain string with error level
+     */
     public static String logLevel(String logLine) {
+    	
     	if (logLine.contains("[INFO]")) {
     		return "info";
     	} else if (logLine.contains("[WARNING]")) {
@@ -18,17 +48,13 @@ public class LogLevels {
     	} else {
     		return "unknown error type";
     	}
-    	
-        
     }
 
+    
+    /**
+     * Return reformatted error message
+     */
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
-    }
-
-    public static void main(String[] args) {
-    	System.out.println(message("[WARNING]: Invalid operation\n\r"));
-    	System.out.println(logLevel("[WARNING]: Invalid operation"));
-    	System.out.println("Hello");
+        return message(logLine) + " (" + logLevel(logLine) + ")";
     }
 }
