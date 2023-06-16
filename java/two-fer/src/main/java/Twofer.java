36b60800368199c761e56f3bdceb09fc89e36c6e
Juha Rainto <juha.rainto@symbio.com>
2023-06-16 19:58:19 +0300

    Started Blackjack exercise

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,5 +1,5 @@
 public class Twofer {
-    public static String twofer(String name) {
+    public String twofer(String name) {
         return name == "" || name == null ? "One for you, one for me." : "One for " + name + ", one for me.";
     }
 }
