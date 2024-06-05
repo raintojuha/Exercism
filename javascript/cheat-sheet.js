4b4f8182d97a4d61321ad945fdee626172e97f0b
Juha Rainto <juha.rainto@symbio.com>
2024-06-05 21:40:47 +0300

    Started JavaScript learning path. Did a few exercises

---
+++ javascript/cheat-sheet.js
@@ -0,0 +1,27 @@
+/**
+ * CHEAT SHEET
+ * I'll gather useful conepts here from the excercises
+ */
+
+console.log("Hello World!");
+
+
+// Using functions, constants and variables in other files
+export const THIS_IS_A_CONSTANT = 0;
+
+export function returnsTrue(){
+    return true;
+}
+
+import {
+    // eslint-disable-next-line import/named
+    THIS_IS_A_CONSTANT,
+    returnsTrue
+  } from './cheat-sheet.js';
+
+
+// Single line boolean checks
+// (if statement) ? then : else
+return knightIsAwake ? false : true;
+return (petDogIsPresent && !archerIsAwake) || (!petDogIsPresent && !archerIsAwake && !knightIsAwake && prisonerIsAwake) ? true : false;
+
